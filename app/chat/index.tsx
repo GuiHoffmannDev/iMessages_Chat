c5f82c349aa66dec3f114108cfa7a7bf8d21cebc
guihoffmann <guihoffmann7@gmail.com>
2023-07-13 21:37:42 -0300

    Private messages

--- app/chat/index.tsx
+++ app/chat/index.tsx
@@ -2,11 +2,19 @@ import { useRouter } from 'expo-router';
 import { useState } from 'react';
 import { Channel as ChannelType } from 'stream-chat';
 import { ChannelList, Channel, MessageList, MessageInput } from 'stream-chat-expo';
+import { useAuth } from '../../src/context/auth';
 
 const ChatScreen = () => {
   const router = useRouter();
+  const {user} = useAuth();
+
+  const isPrivate = {type: 'messaging', members: {$in: [user.id.toString()] }};
+  const isPublic = {type: 'livestream'}
+
   return (
-    <ChannelList onSelect={(channel) => router.push(`/chat/channel/${channel.id}`)} />
+    <ChannelList
+      filters={{ $or: [isPrivate, isPublic ]}}
+     onSelect={(channel) => router.push(`/chat/channel/${channel.id}`)} />
   );
 };
 
--- src/components/UserListItem.tsx
+++ src/components/UserListItem.tsx
@@ -1,14 +1,22 @@
 import { View, Text, Pressable } from 'react-native'
-import { User } from '../context/auth'
+import { User, useAuth } from '../context/auth'
 import { useChatContext } from 'stream-chat-expo'
+import { useRouter } from 'expo-router';
 
 
 const UserListItem = ({user}: {user: User }) => {
     const {client} = useChatContext();
+    const {user: me} = useAuth();
+    const router = useRouter();
 
-    const startChannel = () => {
-        const channel = client.channel('messaging', )
-    }
+    const startChannel = async () => {
+        const channel = client.channel('messaging', {
+            members: [user.id.toString(), me.id.toString()],
+        });
+        await channel.watch();
+
+        router.push(`/chat/channel/${channel.id}`);
+    };
 
   return (
     <Pressable onPress={startChannel} style={{ backgroundColor: 'white', margin: 5, marginVertical: 3, padding: 15, borderRadius: 5}}>
