0b22ae4cca2fa60059b919799c5e26362a6f89c8
guihoffmann <guihoffmann7@gmail.com>
2023-07-13 21:13:53 -0300

    test commit

--- src/components/UserListItem.tsx
+++ src/components/UserListItem.tsx
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable } from 'react-native'
 import { User } from '../context/auth'
+import { useChatContext } from 'stream-chat-expo'
 
 
 const UserListItem = ({user}: {user: User }) => {
+    const {client} = useChatContext();
+
+    const startChannel = () => {
+        const channel = client.channel('messaging', )
+    }
+
   return (
-    <View style={{ backgroundColor: 'white', margin: 5, marginVertical: 3, padding: 15, borderRadius: 5}}>
+    <Pressable onPress={startChannel} style={{ backgroundColor: 'white', margin: 5, marginVertical: 3, padding: 15, borderRadius: 5}}>
       <Text>{user.name}</Text>
-    </View>
+    </Pressable>
   )
 }
 
